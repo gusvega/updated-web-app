2e5b60211733fab804fe5964fa3ee26060385999
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-05 03:52:12 +0000

    Deploy guxtav/updated-web-app to github.com/guxtav/updated-web-app.git:gh-pages

---
+++ precache-manifest.b3c4a223edb6daad0f54af172909fd67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27086fb2fe4ba05dec16",
+    "url": "/updated-web-app/static/css/main.a0c594b8.chunk.css"
+  },
+  {
+    "revision": "27086fb2fe4ba05dec16",
+    "url": "/updated-web-app/static/js/main.27086fb2.chunk.js"
+  },
+  {
+    "revision": "ff4a760c6ebb413c10bb",
+    "url": "/updated-web-app/static/js/runtime~main.ff4a760c.js"
+  },
+  {
+    "revision": "4be2ddedbe2bacebaaea",
+    "url": "/updated-web-app/static/js/2.4be2dded.chunk.js"
+  },
+  {
+    "revision": "b3a2a438755562cc6b4a34b229ef7df1",
+    "url": "/updated-web-app/index.html"
+  }
+];
